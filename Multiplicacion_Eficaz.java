276f14fa28fd8cd8617f84aeaf244a7c9d109055
Miguel Barcenas <hernandezbarcenasmiguelangel@gmail.com>
2020-10-19 09:37:46 -0500

    Add files via upload
    Tarea 3, multiplicación eficaz y eficiente.

---
+++ Multiplicacion_Eficaz.java
@@ -0,0 +1,19 @@
+/*
+ * Universidad Nacional Autónoma de México
+ * Facultad de Estudios Superiores Aragón
+ * Hernández Bárcenas Miguel Ángel
+ */
+
+/**
+ *
+ * @author Miguel Angel
+ */
+public class Multiplicacion_Eficaz {
+    public static void main(String[] args) {	
+         for(int i=1; i<=100; i++){	
+            for(int j=1; j<=100; j++){	
+                System.out.println(i + " x " + j + " = " + (i*j));	
+            }	
+        }	
+    }
+}
